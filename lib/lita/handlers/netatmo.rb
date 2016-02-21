d9a5858a144b92e1d6f37a684c961ff580c55d3f
Nassim Ait boufrad <nassim.aitboufrad@francetv.fr>
2016-02-21 16:05:47 +0100

    use oauth2 gem

--- lib/lita/handlers/netatmo.rb
+++ lib/lita/handlers/netatmo.rb
@@ -6,7 +6,8 @@ module Lita
       config :username,      type: String, required: true
       config :password,      type: String, required: true
 
-      route /^(netatmo|air|空気)/, :air, command: false, help: { "netatmo|air|空気" => "Fetch sensor data from netatmo." }
+      route /^netatmo air$/, :air, command: false, help: { "netatmo air" => "Fetch sensor data from netatmo." }
+
       def air(response)
         response.reply build_message(stations_data)
       end
@@ -18,8 +19,8 @@ module Lita
         inside  = device['dashboard_data']
         outside = device['modules'].first['dashboard_data']
 
-        "[room] #{inside['Temperature']} ℃, #{inside['Humidity']} %, #{inside['Pressure']} hPa, CO2: #{inside['CO2']} ppm\n" +
-        "[outside] #{outside['Temperature']} ℃, #{outside['Humidity']} %"
+        message = "[inside] #{inside['Temperature']} °C, #{inside['Humidity']} %, #{inside['Pressure']} hPa, CO2: #{inside['CO2']} ppm\n"
+        message += "[outside] #{outside['Temperature']} °C, #{outside['Humidity']} %" if !outside.blank?
       end
 
       # https://dev.netatmo.com/doc/methods/getstationsdata
@@ -32,17 +33,13 @@ module Lita
         token = redis.get 'access_token'
         return token if token
 
-        # https://dev.netatmo.com/doc/authentication/usercred
-        response = http.post('https://api.netatmo.com/oauth2/token',
-                             grant_type:    'password',
-                             client_id:     config.client_id,
-                             client_secret: config.client_secret,
-                             username:      config.username,
-                             password:      config.password,
-                             scope:          'read_station')
-        auth = MultiJson.load(response.body)
-        token = auth['access_token']
-        redis.setex 'access_token', (auth['expires_in'] - 10), token
+        client = OAuth2::Client.new(config.client_id, config.client_secret, :site => 'https://api.netatmo.net/')
+        client.options= {:authorize_url=>'/oauth2/authorize', :token_url=>'/oauth2/token', :token_method=>:post, :connection_opts=>{}, :max_redirects=>5, :raise_errors=>true}
+        token = client.password.get_token(config.username, config.password)
+
+        auth  = token.to_hash
+        token = auth[:access_token]
+        redis.setex 'access_token', (auth[:expires_in] - 10), token
         token
       end
 
